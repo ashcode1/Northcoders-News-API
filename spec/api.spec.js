787c89de69a8012819b4ab2adc42f4f6bb69ec6a
Ashley Hopkins <ashleyhopkins@Ashleys-MacBook-Pro.local>
2017-10-09 12:11:19 +0100

    [models/][seed/][spec/][server.js] {API GET/ responds with status code 200}

---
+++ spec/api.spec.js
@@ -0,0 +1,38 @@
+process.env.NODE_ENV = 'test';
+const {expect} = require('chai');
+const request = require('supertest');
+const server = require('../server');
+const saveTestData = require('../seed/test.seed');
+const config = require('../config');
+const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+describe('API', function () {
+  let usefulIds;
+  beforeEach(function (done) {
+    this.timeout(4000);
+    mongoose.connection.dropDatabase()
+      .then(saveTestData)
+      .then(data => {
+        usefulIds = data;
+        console.log(usefulIds);
+        done();
+      })
+      .catch(done);
+  });
+
+  describe('GET /', function () {
+    it('responds with status code 200', function (done) {
+      request(server)
+        .get('/')
+        .end((err, res) => {
+          if (err) done(err);
+          else {
+            expect(res.status).to.equal(200);
+            done();
+          }
+        });
+    });
+  });
+});
