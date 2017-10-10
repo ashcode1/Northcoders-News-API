057e7e02398b5a7f08a19ed06a9bd2e01313f459
Ashley Hopkins <ashleyhopkins@Ashleys-MacBook-Pro.local>
2017-10-10 21:48:37 +0100

    [putCommentVoteCount] {add method to controllers, increase & decrease votes on comments}

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const {
   getAllCommentsForArticle,
   postNewCommentToArticle,
   getUserProfile,
-  putVoteCount
+  putVoteCount,
+  putCommentVoteCount
 } = require('./controllers/controllers');
 
 mongoose.connect(db, function (err) {
@@ -42,6 +43,7 @@ app.get('/api/users/:username', getUserProfile);
 app.post('/api/articles/:article_id/comments', postNewCommentToArticle);
 
 app.put('/api/articles/:article_id',putVoteCount);
+app.put('/api/comments/:comment_id',putCommentVoteCount);
 
 app.use('/api', function () {});
 
