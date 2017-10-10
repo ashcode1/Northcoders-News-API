42f6220d0244a0ffe914c7f664befbafc1d6a28e
Ashley Hopkins <ashleyhopkins@Ashleys-MacBook-Pro.local>
2017-10-10 23:34:35 +0100

    [server] {include cors]

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const config = require('./config');
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
+const cors = require('cors');
 
 const apiRouter = require('./routes/api');
 
@@ -20,6 +21,8 @@ mongoose.connect(db, function (err) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use('/api', apiRouter);
 
