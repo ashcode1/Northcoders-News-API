dd80bfd72068938b884cb5090c3d3d548a06f816
Ashley Hopkins <ashleyhopkins@Ashleys-MacBook-Pro.local>
2017-10-10 21:04:11 +0100

    [getArticlesByTopic] {filter articles by topic}

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,27 +1,17 @@
-
-
 process.env.NODE_ENV = 'test';
 const {expect} = require('chai');
 const request = require('supertest');
 const server = require('../server');
 const saveTestData = require('../seed/test.seed');
 const config = require('../config');
-// const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
 describe('API', function () {
-  let usefulIds;
-  beforeEach(function (done) {
-    this.timeout(0);
+  // let usefulIds;
+  beforeEach((done) => {
     mongoose.connection.dropDatabase()
-      .then(saveTestData)
-      .then(data => {
-        usefulIds = data;
-        console.log(usefulIds);
-        done();
-      })
-      .catch(done);
+      .then(() => saveTestData(db, done));
   });
   describe('GET /', function () {
     it('responds with status code 200', function (done) {
